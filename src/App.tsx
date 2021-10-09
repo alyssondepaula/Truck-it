6103fef083d0a0f5ba30f52064ceea87437002f5
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-10-09 12:55:01 -0300

    :sparkles: feat: adicionado serviço fake de busca dos dados

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Map from './components/Map';
 import './styles/globals.scss';
 import styles from './styles.module.scss'
+import { Box } from './components/Box';
+
 
 function App() {
+
   
 
   return (
     <div className={styles.container}>
+    <Box/>
     <Map/>
     </div>
   );
---
+++ src/services/getDataFake.ts
@@ -0,0 +1,8 @@
+import { TGpsData } from '../types_interfaces/gpsData';
+import frontend_data_gps from './DATAS/frontend_data_gps.json'
+
+export function getDataFake(): TGpsData {
+
+    return frontend_data_gps;
+    
+}
