6ab390cca4f76890c4726515ce8ab0dff293006d
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-10-09 12:46:27 -0300

    :sparkles: feat: box de pesquisa adicionado

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss'
+
+export function Box() {
+    return (
+    <div className={styles.container}>
+         
+         <div>
+             <text>Exibir rotas</text>
+         </div>
+
+        <form>    
+        </form> 
+
+    </div>
+    );
+}
