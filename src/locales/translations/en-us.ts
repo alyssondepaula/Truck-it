73915b85d04f2197e51fbb29bbc57635b8766bb1
alyssondepaula <alyssonjuniordepaula@gmail.com>
2021-10-10 22:19:29 -0300

    :recycle: feat: traduções atualizadas

--- src/locales/translations/en-us.ts
+++ src/locales/translations/en-us.ts
@@ -1,6 +1,15 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
     translations: {
+      flagSelector: {
+        selectLanguage: 'Select your language:'
+      },
+      sideMenu: {
+        routes: 'Routes',
+        myCars: 'My Cars',
+        settings: 'Settings',
+        logout: 'Logout'
+      },
       box: {
         heading: 'Display routes!',
         subHeading: 'Select the cars departure time:'
--- src/locales/translations/pt-br.ts
+++ src/locales/translations/pt-br.ts
@@ -1,6 +1,15 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
     translations: {
+      flagSelector: {
+        selectLanguage: 'Selecione o Idioma:'
+      },
+      sideMenu: {
+        routes: 'Rotas',
+        myCars: 'Minha Frota',
+        settings: 'Configurações',
+        logout: 'Sair'
+      },
       box: {
         heading: 'Exibir rotas!',
         subHeading: 'Selecione a hora de partida do carro:'
